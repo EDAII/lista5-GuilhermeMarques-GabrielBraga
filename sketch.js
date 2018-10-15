947989804f85e776d1045af8f3a176a174f03ce9
Guilherme Marques Rosa <marquesguilherme3@gmail.com>
2018-10-15 14:00:13 -0300

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    // put setup code here
+    createCanvas(600, 400)
+    background("#ccc")
+}
+
+function draw() {
+    // put drawing code here
+    ellipse(width/2, height/2, 32, 32)
+}
